09dff77e635a61922808bf327b9c5630407eb917
CryptoLorn <lorn.crypto@gmail.com>
2023-06-13 13:13:38 +0300

    fix are there rights to add brands/types

--- server/src/brands/brands.controller.ts
+++ server/src/brands/brands.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
 
 import { BrandsService } from "./brands.service";
 import { IBrand } from "./interfaces/brand.interface";
 import { CreateDto } from "./dto/create.dto";
+import { Roles } from "../core/guards/rolesAuth.decorator";
+import { constant } from "../core/constants/constant";
+import { RoleGuard } from "../core/guards/role.guard";
 
 @Controller("brands")
 export class BrandsController {
     constructor(private brandService: BrandsService) {}
 
+    @Roles(constant.ADMIN)
+    @UseGuards(RoleGuard)
+    @UseGuards(AuthGuard("jwt"))
     @Post()
     async create(@Body() brand: CreateDto): Promise<IBrand> {
         return await this.brandService.create(brand);
--- server/src/types/types.controller.ts
+++ server/src/types/types.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
 
 import { TypesService } from "./types.service";
 import { CreateDto } from "./dto/create.dto";
 import { IType } from "./interfaces/type.interface";
+import { Roles } from "../core/guards/rolesAuth.decorator";
+import { constant } from "../core/constants/constant";
+import { RoleGuard } from "../core/guards/role.guard";
 
 @Controller("types")
 export class TypesController {
     constructor(private typeService: TypesService) {}
 
+    @Roles(constant.ADMIN)
+    @UseGuards(RoleGuard)
+    @UseGuards(AuthGuard("jwt"))
     @Post()
     async create(@Body() type: CreateDto): Promise<IType> {
         return await this.typeService.create(type);
